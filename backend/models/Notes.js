5acb1578cb4825b5317a7de125d2e0e3d9a0891e
uzxr <uzersayed9@gmail.com>
2022-11-19 15:14:14 +0530

    made a schema

---
+++ backend/models/Notes.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const NotesSchema = new Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true,
+    },
+    tag:{
+        type:String,
+        default: "General",
+    },
+    date:{
+        type:Date,
+        default:date.now
+    },
+
+  });
+
+  module.exports = mongoose.model('notes', NotesSchema);
