f855f9e9b322584aa73261ffb62048820edc2a8b
uzxr <uzersayed9@gmail.com>
2023-01-05 14:52:59 +0530

    added icons to notes

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,7 +8,7 @@ const Home = () => {
         <h2>Add a note</h2>
         <form className="my-3">
           <div className="mb-3">
-            <label for="exampleInputEmail1" className="form-label">
+            <label htmlFor="exampleInputEmail1" className="form-label">
               Email address
             </label>
             <input
@@ -22,7 +22,7 @@ const Home = () => {
             </div>
           </div>
           <div className="mb-3">
-            <label for="exampleInputPassword1" className="form-label">
+            <label htmlFor="exampleInputPassword1" className="form-label">
               Password
             </label>
             <input
@@ -37,7 +37,7 @@ const Home = () => {
               className="form-check-input"
               id="exampleCheck1"
             />
-            <label className="form-check-label" for="exampleCheck1">
+            <label className="form-check-label" htmlFor="exampleCheck1">
               Check me out
             </label>
           </div>
