afbfe9e768b1d388900ccf456a2a55677f95af7d
uzxr <uzersayed9@gmail.com>
2023-01-05 14:22:29 +0530

    Noteitem added to separate Note component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,7 @@
-import React, { useContext } from "react";
-import noteContext from "../context/notes/noteContext"
+import Notes from "./Notes";
 
 const Home = () => {
-  const context = useContext(noteContext);
-  const {notes, setNotes} = context 
+
   return (
     <div>
       <div className="container my-3">
@@ -48,12 +46,8 @@ const Home = () => {
           </button>
         </form>
       </div>
-      <div className="container my-3">
-        <h2>Your Notes</h2>
-        {notes.map((note) =>{
-          return note.title;
-        })}
-      </div>
+
+      <Notes/>
     </div>
   );
 };
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -11,6 +11,60 @@ const NoteState = (props) => {
       "tag": "personal",
       "date": "2022-12-31T14:55:16.428Z",
       "__v": 0
+    },
+    {
+      "_id": "63b04d54e12f1d81cb6c3a46",
+      "user": "6378fb251a6b05a032ee5252",
+      "title": "My Title",
+      "description": "wake up",
+      "tag": "personal",
+      "date": "2022-12-31T14:55:16.428Z",
+      "__v": 0
+    },
+    {
+      "_id": "63b04d54e12f1d81cb6c3a46",
+      "user": "6378fb251a6b05a032ee5252",
+      "title": "My Title",
+      "description": "wake up",
+      "tag": "personal",
+      "date": "2022-12-31T14:55:16.428Z",
+      "__v": 0
+    },
+    {
+      "_id": "63b04d54e12f1d81cb6c3a46",
+      "user": "6378fb251a6b05a032ee5252",
+      "title": "My Title",
+      "description": "wake up",
+      "tag": "personal",
+      "date": "2022-12-31T14:55:16.428Z",
+      "__v": 0
+    },
+    {
+      "_id": "63b04d54e12f1d81cb6c3a46",
+      "user": "6378fb251a6b05a032ee5252",
+      "title": "My Title",
+      "description": "wake up",
+      "tag": "personal",
+      "date": "2022-12-31T14:55:16.428Z",
+      "__v": 0
+    },
+    {
+      "_id": "63b04d54e12f1d81cb6c3a46",
+      "user": "6378fb251a6b05a032ee5252",
+      "title": "My Title",
+      "description": "wake up",
+      "tag": "personal",
+      "date": "2022-12-31T14:55:16.428Z",
+      "__v": 0
+    },
+    {
+      "_id": "63b04d54e12f1d81cb6c3a46",
+      "user": "6378fb251a6b05a032ee5252",
+      "title": "My Title",
+      "description": "wake up",
+      "tag": "personal",
+      "date": "2022-12-31T14:55:16.428Z",
+      "__v": 0
     }
   ]
   const [notes, setNotes] = useState(notesInitial)
