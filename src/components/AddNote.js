90ba9ad088b6eb5128e0a16d8bf5d1015a0e7047
uzxr <uzersayed9@gmail.com>
2023-01-06 21:45:56 +0530

    Added a modal for editing the notes

--- src/components/AddNote.js
+++ src/components/AddNote.js
@@ -5,10 +5,12 @@ const AddNote = () => {
   const context = useContext(noteContext);
   const { addNote } = context;
   const [note, setNote] = useState({ title: "", description: "", tag: "default" });
+
   const handleClick = (e) => {
     e.preventDefault();
     addNote(note.title, note.description, note.tag);
   };
+  
   const onChange = (e) => {
     setNote({...note, [e.target.name]: e.target.value });
   };
@@ -41,16 +43,20 @@ const AddNote = () => {
             onChange={onChange}
           />
         </div>
-        <div className="mb-3 form-check">
-          <input
-            type="checkbox"
-            className="form-check-input"
-            id="exampleCheck1"
-          />
-          <label className="form-check-label" htmlFor="exampleCheck1">
-            Check me out
+        <div className="mb-3">
+          <label htmlFor="tag" className="form-label">
+            Tag
           </label>
+          <input
+            type="text"
+            className="form-control"
+            id="tag"
+            name="tag"
+            onChange={onChange}
+          />
         </div>
+
+
         <button type="submit" className="btn btn-primary" onClick={handleClick}>
           Add Note
         </button>
--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -53,7 +53,20 @@ const NoteState = (props) => {
 
 
   //Delete a note
-  const deleteNote = (id) => {
+    const deleteNote = async(id) => {
+      //API call
+      const response = await fetch(`${host}/api/notes/deletenote/${id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          "auth-token":
+            "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjM3OGZiMjUxYTZiMDVhMDMyZWU1MjUyIn0sImlhdCI6MTY2ODkzNDM2OX0.aLhuBZB3xg4WhU5LBWasyJeKfkXHMtCuEPueMYV5xfs",
+        }
+      });
+      const json = response.json();
+      console.log(json)
+
+
     console.log("Deleting the note with id" + id);
     const newNotes = notes.filter((note) => {
       return note._id !== id;
